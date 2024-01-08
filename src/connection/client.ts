f5c0a49cbbf99317d6a701d21a2a314e89fe7cae
Michał Kalinowski <michal@kalinowski.one>
2024-01-08 16:26:25 +0100

    feat: add Client and Session
    Co-authored-by: Kordian Grabowski <kordiangrabowski@gmail.com>

---
+++ src/connection/client.ts
@@ -0,0 +1,35 @@
+import { type RequestBuilderTemplate, type RequestBuilderResponseType } from '../protocol/requests/request-builder';
+import { Session } from './session';
+
+export class Client {
+  private session: Session | null = null;
+
+  constructor(
+    private readonly host: string,
+    private readonly port: number,
+    private readonly clientId: string | null,
+    private readonly clientSoftwareName: string,
+    private readonly clientSoftwareVersion: string
+  ) {}
+
+  public async send<T extends RequestBuilderTemplate<any>>(requestBuilder: T): Promise<RequestBuilderResponseType<T>> {
+    const session = await this.getOrCreateSession();
+
+    return session.send(requestBuilder);
+  }
+
+  private async getOrCreateSession(): Promise<Session> {
+    //todo: consider changing this condition
+    if (this.session === null || !this.session.isHealthy()) {
+      this.session = await Session.create(
+        this.host,
+        this.port,
+        this.clientId,
+        this.clientSoftwareName,
+        this.clientSoftwareVersion
+      );
+    }
+
+    return this.session;
+  }
+}
