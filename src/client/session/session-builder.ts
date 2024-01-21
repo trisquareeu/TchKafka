18385bbf958ba062bcceb7466f5c53f3e251f756
Kordian Grabowski <kordiangrabowski@gmail.com>
2024-01-21 18:01:44 +0100

    feat: add support for authentication methods

--- src/client/session/session-builder.ts
+++ src/client/session/session-builder.ts
@@ -1,18 +1,23 @@
 import { Connection, type SocketFactory } from './connection';
 import { type SessionOptions } from './session-options';
 import { type SupportedApiVersions } from './supported-api-versions';
+import { type Authenticator } from './security';
 import { Session } from './session';
 import { ApiVersionsRequestBuilder } from '../../protocol/requests';
 
 export class SessionBuilder {
   constructor(
     private readonly socketFactory: SocketFactory,
+    private readonly authenticator: Authenticator,
     private readonly clientOptions: SessionOptions
   ) {}
 
   public async newSession(port: number, host: string): Promise<Session> {
     const socket = await this.socketFactory.connect(port, host, this.clientOptions.connectionTimeout);
     const connection = new Connection(socket);
+
+    await this.authenticator.authenticate(connection);
+
     const apiVersions = await this.getApiVersions(connection);
 
     return new Session(connection, apiVersions);
@@ -29,6 +34,8 @@ export class SessionBuilder {
     if (apiVersions.value === null) {
       throw new Error('Received null response');
     }
+
+    //todo: fallback to supported ApiVersions in case of error
 
     return apiVersions.value.reduce<SupportedApiVersions>((acc, { apiKey, minVersion, maxVersion }) => {
       acc[apiKey.value] = { min: minVersion.value, max: maxVersion.value };
