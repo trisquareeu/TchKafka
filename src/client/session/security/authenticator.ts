545c71a7f1a3632fd816f9db7c6d1b98f0cb8593
Kordian Grabowski <kordiangrabowski@gmail.com>
2024-01-21 18:01:44 +0100

    feat: add support for authentication methods

---
+++ src/client/session/security/authenticator.ts
@@ -0,0 +1,9 @@
+import { type Connection } from '../connection';
+
+export interface Authenticator {
+  authenticate(connection: Connection): Promise<void>;
+}
+
+export class NoOpAuthenticator implements Authenticator {
+  public async authenticate(): Promise<void> {}
+}
