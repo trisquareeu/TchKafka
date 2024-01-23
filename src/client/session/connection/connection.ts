f6e390353f7884ef61ac3666a2a5843de730c2f1
Michał Kalinowski <michal@kalinowski.one>
2024-01-23 23:39:49 +0100

    refactor(connection): simplify InflightRequest queue
    also improve readability of request serialization and deserialization

--- src/client/session/connection/connection.ts
+++ src/client/session/connection/connection.ts
@@ -1,14 +1,12 @@
 import { type Socket } from 'net';
 import { type Request, type RequestResponseType } from '../../../protocol/requests';
-import { type ReadBuffer, WriteBuffer } from '../../../protocol/serialization';
+import { ReadBuffer, WriteBuffer } from '../../../protocol/serialization';
 import { CorrelationIdMismatchError } from '../../../protocol/exceptions';
 import { ResponseReader } from './response-reader';
 
 type InflightRequest<T> = {
-  request: Request<T>;
   resolve: (value: T | PromiseLike<T>) => void;
-  reject: (reason?: any) => void;
-  correlationId: number;
+  reject: (reason?: Error) => void;
 };
 
 export class Connection {
@@ -16,44 +14,55 @@ export class Connection {
   private readonly inFlightRequests: InflightRequest<any>[] = [];
 
   constructor(private readonly socket: Socket) {
-    this.responseReader = new ResponseReader(this.handleResponse.bind(this));
+    this.responseReader = new ResponseReader(this.resolveResponse.bind(this));
     socket.on('data', (stream) => this.responseReader.maybeReadResponse(stream));
   }
 
   public async send<T extends Request<any>>(request: T): Promise<RequestResponseType<T>> {
-    const serializedRequest = new WriteBuffer();
-    await request.serialize(serializedRequest);
+    const serializedRequest = await this.serializeRequest(request);
 
-    const header = Buffer.alloc(4);
-    header.writeInt32BE(serializedRequest.toBuffer().length);
-    this.socket.write(Buffer.concat([header, serializedRequest.toBuffer()]));
-    //todo: implement problem handling
+    const response = await this.doSend(serializedRequest);
 
-    return new Promise<RequestResponseType<T>>((resolve, reject) => {
-      this.inFlightRequests.push({ request, resolve, reject, correlationId: request.header.correlationId.value });
-    });
+    return this.deserializeResponse(response, request);
   }
 
   public isHealthy(): boolean {
     return this.socket.writable && this.socket.readable;
   }
 
-  private handleResponse(buffer: ReadBuffer): void {
+  private async serializeRequest(request: Request<any>): Promise<Buffer> {
+    const serializedRequest = new WriteBuffer();
+    await request.serialize(serializedRequest);
+
+    return serializedRequest.toBuffer();
+  }
+
+  private deserializeResponse<T>(response: Buffer, request: Request<T>): T {
+    const readBuffer = new ReadBuffer(response);
+    const { correlationId } = request.ExpectedResponseHeaderClass.deserialize(readBuffer);
+    if (correlationId.value !== request.header.correlationId.value) {
+      throw new CorrelationIdMismatchError('Received response with unexpected correlation ID');
+    }
+
+    return request.ExpectedResponseDataClass.deserialize(readBuffer);
+  }
+
+  private async doSend(data: Buffer): Promise<Buffer> {
+    const header = Buffer.alloc(4);
+    header.writeInt32BE(data.length);
+    this.socket.write(Buffer.concat([header, data]));
+
+    return new Promise((resolve, reject) => {
+      this.inFlightRequests.push({ resolve, reject });
+    });
+  }
+
+  private resolveResponse(buffer: Buffer): void {
     const oldestRequest = this.inFlightRequests.shift();
     if (oldestRequest === undefined) {
+      // TODO: Close connection and cancel all inflight requests
       throw new Error('Received response without a matching request');
     }
-
-    try {
-      const { correlationId } = oldestRequest.request.ExpectedResponseHeaderClass.deserialize(buffer);
-      if (correlationId.value !== oldestRequest.correlationId) {
-        throw new CorrelationIdMismatchError('Received response with unexpected correlation ID');
-      }
-
-      const data = oldestRequest.request.ExpectedResponseDataClass.deserialize(buffer);
-      oldestRequest.resolve(data);
-    } catch (error) {
-      oldestRequest.reject(error);
-    }
+    oldestRequest.resolve(buffer);
   }
 }
