1e1270afa81ab288ce9e9381182056b9272a97e0
Michał Kalinowski <michal@kalinowski.one>
2025-09-28 00:29:13 +0200

    fix: run only unit tests in unit test step

--- packages/client/jest-unit.config.ts
+++ packages/client/jest-unit.config.ts
@@ -4,7 +4,8 @@ import jestConfig from './jest.config';
 const config: JestConfigWithTsJest = {
   ...jestConfig,
   reporters: ['default', ['jest-junit', { outputName: 'junit-unit.xml' }]],
-  coverageDirectory: './coverage/unit'
+  coverageDirectory: './coverage/unit',
+  testMatch: ['<rootDir>/src/**/*.spec.ts']
 };
 
 export default config;
